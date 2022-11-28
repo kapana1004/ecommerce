2461159a84cc0f418fd2d283c1fbac21852a72ae
Mac <mac@Macs-MacBook-Pro.local>
2022-11-28 14:15:31 +0400

    add files

---
+++ src/components/Menubar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Menubar() {
+  return (
+    <div>
+      <div className=" flex flex-row ">
+        <img src="./images/icon-menu.svg" alt="menu" />
+        <h1 className=""> sneakers</h1>
+        <img src="./images/icon-cart.svg" alt="cart" />
+        <img src="./images/image-avatar.png" alt="avatar" />
+      </div>
+    </div>
+  );
+}
